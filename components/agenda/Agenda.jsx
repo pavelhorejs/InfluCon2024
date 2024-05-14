b03fd023bf2116b847a4966cd80f4e678a2b4a8a
pavelH <horejs.ho@gmail.com>
2024-05-14 11:39:15 +0200

    first

--- components/agenda/Agenda.jsx
+++ components/agenda/Agenda.jsx
@@ -9,35 +9,45 @@ const Agenda = () => {
       </Revealx>
       <Revealx delay={0.2}>
         {" "}
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-10 bg-[#F00065] py-9 px-9 mb-8 redGlow">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-10 bg-[#F00065] py-9 px-9 mb-8 redGlow redCard">
+          <div className="redItem1"></div>
+          <div className="redItem2"></div>
           <div className="">
             <h3 className="text-4xl font-bold">10:00-10:45</h3>
-            <p className="font-bold">Registrace, káva & networking</p>
+            <p>-</p>
+            <p className="">Registrace, káva & networking</p>
           </div>
           <div className="">
             <h3 className="text-4xl font-bold">11:00-13:00</h3>
-            <p className="font-bold">Blok I a II</p>
+            <p>-</p>
+            <p className="">Blok I a II</p>
           </div>
           <div className="">
             <h3 className="text-4xl font-bold">13:00-14:00</h3>
-            <p className="font-bold">Oběd</p>
+            <p>-</p>
+            <p className="">Oběd</p>
           </div>
         </div>
       </Revealx>
       <Revealx delay={0.4}>
         {" "}
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-10 bg-[#F00065] py-9 px-9 redGlow">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-10 bg-[#F00065] py-9 px-9 redGlow redCard2">
+          <div className="redItem3"></div>
+          <div className="redItem4"></div>
           <div className="">
             <h3 className="text-4xl font-bold">14:00-16:00</h3>
-            <p className="font-bold">Blok III a IV</p>
+            <p>-</p>
+            <p className="">Blok III a IV</p>
           </div>
           <div className="">
             <h3 className="text-4xl font-bold">16:00-18:00</h3>
-            <p className="font-bold">Networking a setkání s influencery</p>
+            <p>-</p>
+            <p className="">Networking a setkání s influencery</p>
           </div>
           <div className="">
             <h3 className="text-4xl font-bold">21:00</h3>
-            <p className="font-bold">After party</p>
+            <p>-</p>
+            <p className="">After party</p>
           </div>
         </div>
       </Revealx>
