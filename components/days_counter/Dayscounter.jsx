435b0dbc96887ab598ca531d7d04df8f529c474a
pavelH <horejs.ho@gmail.com>
2024-05-14 12:33:21 +0200

    first

--- components/days_counter/Dayscounter.jsx
+++ components/days_counter/Dayscounter.jsx
@@ -1,16 +1,12 @@
 const Dayscounter = () => {
-  const entryDate = new Date("10/10/2024");
+  const entryDate = new Date("10/07/2024");
   const currentDate = new Date();
   const timeDiff = entryDate.getTime() - currentDate.getTime();
   const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
   return (
     <div className="text-black font-bold">
-      {daysDiff > 0 ? (
-        <p>Začíná za {daysDiff} dní</p>
-      ) : (
-        <p>Countdown has ended</p>
-      )}
+      {daysDiff > 0 ? <p>Začíná za {daysDiff} dní</p> : <p>Akce již začala</p>}
     </div>
   );
 };
