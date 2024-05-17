8828d06a801c00e13654f583071fe735d32dfa29
pavelH <horejs.ho@gmail.com>
2024-05-17 12:13:51 +0200

    push to main

--- app/[locale]/(pages)/cookies/page.jsx
+++ app/[locale]/(pages)/cookies/page.jsx
@@ -1,114 +1,65 @@
+"use client";
 import styles from "../gdpr/gdpr.module.scss";
+import { useTranslation } from "react-i18next";
 const Page = () => {
+  const { t } = useTranslation();
   return (
     <>
       <div className="max-w-6xl mx-auto mt-32 lg:mt-48 pb-14 px-3">
-        <h2 className="pb-[24px]">Soubory Cookies</h2>
+        <h2 className="pb-[24px]">{t("CookiesHeadline")}</h2>
         <p>
-          Tyto Zásady cookies se vztahují na občany a osoby s trvalým pobytem v
-          Evropském hospodářském prostoru. <br /> <br />
+          {t("subHeadline")} <br /> <br />
           <span className={styles.bold}>
-            1. Úvod <br />
+            {t("cookiesP1")} <br />
           </span>
-          Naše webové stránky https://influcon.cz (dále jen „web“) používají
-          cookies a další související technologie (pro usnadnění jsou všechny
-          technologie označovány jako „cookies“). Cookies také vkládají třetí
-          strany, které jsme zapojili. V níže uvedeném dokumentu vás informujeme
-          o používání cookies na našem webu.{" "}
+          {t("cookiesP1Text")}
           <span className={styles.bold}>
             {" "}
             <br /> <br />
-            2. Co jsou soubory cookies? <br />
+            {t("cookiesP2")} <br />
           </span>
-          Soubor cookie je malý jednoduchý soubor, který je odeslán spolu se
-          stránkami tohoto webu a uložen prohlížečem na pevný disk počítače nebo
-          jiného zařízení. Informace v nich uložené mohou být vráceny našim
-          serverům nebo serverům příslušných třetích stran během následné
-          návštěvy.
+          {t("cookiesP2Text")}
           <span className={styles.bold}>
             {" "}
             <br /> <br />
-            3. Co jsou skripty? <br />
+            {t("cookiesP3")} <br />
           </span>
-          Skript je část programového kódu, který slouží k tomu, aby naše webové
-          stránky fungovaly správně a interaktivně. Tento kód je spuštěn na
-          našem serveru nebo na vašem zařízení. <br /> <br />
+          {t("cookiesP3Text")} <br /> <br />
           <span className={styles.bold}>
-            4. Co je to webový maják? <br />
+            {t("cookiesP4")} <br />
           </span>
-          Webový maják (nebo pixelová značka) je malý, neviditelný kus textu
-          nebo obrázku na webu, který se používá ke sledování provozu na webu.
-          Za tímto účelem jsou různá data o vás ukládána pomocí webových majáků.{" "}
+          {t("cookiesP4Text")}
           <br /> <br />
           <span className={styles.bold}>
-            5. Cookies <br />
+            {t("cookiesP5")} <br />
           </span>
           <span className={styles.bold}>
-            5.1 Technické nebo funkční soubory cookies <br />
+            {t("cookiesP5_1")} <br />
           </span>
-          Některé soubory cookies zajišťují, že určité části webu fungují
-          správně a že vaše uživatelské preference zůstávají známé. Umístěním
-          funkčních souborů cookies usnadňujeme návštěvu našich webových
-          stránek. Tímto způsobem nemusíte při návštěvě našich webových stránek
-          opakovaně zadávat stejné informace a například položky zůstanou v
-          nákupním košíku, dokud nezaplatíte. Tyto cookies můžeme umístit bez
-          vašeho souhlasu. <br />
+          {t("cookiesP5_1Text")} <br />
           <span className={styles.bold}>
-            5.2 Statistické cookies <br />
+            {t("cookiesP5_2")} <br />
           </span>
-          Statistické soubory cookies využíváme k optimalizaci webových stránek
-          pro naše uživatele. Díky těmto statistickým cookies získáváme přehled
-          o používání našich webových stránek. <br />
+          {t("cookiesP5_2Text")}. <br />
           <span className={styles.bold}>
-            5.3 Reklamní soubory cookies <br />
+            {t("cookiesP5_3")} <br />
           </span>
-          Reklamní soubory cookies, umožňují přizpůsobit reklamy a pomáhají
-          získávat informace o výsledcích kampaně. To se děje na základě
-          profilu, který vytvoříme na základě vašeho kliknutí a surfování na
-          https://influcon.cz a mimo něj. S těmito cookies jste jako návštěvník
-          webu spojeni s jedinečným ID, takže se stejná reklama například
-          nezobrazuje více než jednou. <br />
-          <span className={styles.bold}>
-            5.4 Marketingové/Sledovací cookies{" "}
-          </span>
+          {t("cookiesP5_3Text")} <br />
+          <span className={styles.bold}>{t("cookiesP5_4")}</span>
           <br />
-          Marketingové/sledovací cookies jsou soubory cookies nebo jakákoli jiná
-          forma místního úložiště, které se používají k vytváření uživatelských
-          profilů k zobrazování reklamy nebo ke sledování uživatele na tomto
-          webu nebo na několika webech pro podobné marketingové účely. <br />{" "}
-          Protože tyto soubory cookies jsou označeny jako sledovací, žádáme vás
-          o jejich povolení. <br /> <br />
-          <span className={styles.bold}>6. Umístěné cookies </span>
-          <br /> Google Analytics (Statistické) <br /> Stripe_mid (Funkční){" "}
-          <br /> Stripe_sid (Funkční)
+          {t("cookiesP5_4Text")} <br /> <br />
+          <span className={styles.bold}>{t("cookiesP6")}</span>
+          <br /> {t("cookiesP6_Text1")} <br /> {t("cookiesP6_Text2")}
+          <br /> {t("cookiesP6_Text3")} <br /> {t("cookiesP6_Text4")}
           <br /> <br />
           <span className={styles.bold}>
-            7. Souhlas <br />
+            {t("cookiesP7")} <br />
           </span>
-          Když poprvé navštívíte náš web, ukážeme vám vyskakovací okno s
-          vysvětlením o cookies. Jakmile kliknete na "Povolit", vyjadřujete svůj
-          souhlas s používáním souborů cookies a doplňků popsaných v těchto
-          Zásadách cookies. Používání cookies můžete zakázat pomocí svého
-          prohlížeče, ale mějte na paměti, že naše webové stránky již nemusí
-          fungovat správně. <br /> <br />
-          <span className={styles.bold}>
-            8. Povolení/zakázání a odstranění cookies
-          </span>{" "}
-          <br />
-          Pomocí internetového prohlížeče můžete automaticky nebo ručně mazat
-          soubory cookies. Můžete také určit, že některé soubory cookies nemusí
-          být umístěny. Další možností je změnit nastavení internetového
-          prohlížeče tak, aby se vám při každém uložení souboru cookies
-          zobrazila zpráva. Další informace o těchto možnostech naleznete v
-          Nápovědě vašeho prohlížeče. Vezměte prosím na vědomí, že náš web
-          nemusí fungovat správně, pokud jsou deaktivovány všechny cookies.
-          Pokud cookies smažete ve svém prohlížeči, budou znovu umístěny po
-          vašem souhlasu, když znovu navštívíte naše webové stránky. <br />{" "}
-          <br />
-          <span className={styles.bold}>9. Kontatkní údaje</span> <br />
-          V případě dotazů nebo komentářů ohledně našich zásad týkajících se
-          souborů cookies a tohoto prohlášení nás prosím kontaktujte pomocí
-          následujících kontaktních údajů:
+          {t("cookiesP7_Text")} <br /> <br />
+          <span className={styles.bold}>{t("cookiesP8")}</span> <br />
+          {t("cookiesP8_Text")} <br /> <br />
+          <span className={styles.bold}>{t("cookiesP9")}</span> <br />
+          {t("cookiesP9_Text")}
           <br />
           <br />
           <a className="underline" href="mailto:info@woo.cz">
