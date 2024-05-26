016b23a2f08103ffa9c2f1ac17697e82d33b5cf7
pavelH <horejs.ho@gmail.com>
2024-05-26 18:25:22 +0200

    updates meta data + faq

--- app/[locale]/(pages)/dashboard/page.jsx
+++ app/[locale]/(pages)/dashboard/page.jsx
@@ -1,5 +1,5 @@
 import Dashboard from "/components/pagesrepo/dashboard/Dashboard";
-import { metadataCs, metadataEn } from "/app/metadata/contact";
+import { metadataCs, metadataEn } from "/app/metadata/dashabord";
 
 export async function generateMetadata({ params: { locale } }) {
   const metadata = locale === "cs" ? metadataCs : metadataEn;
