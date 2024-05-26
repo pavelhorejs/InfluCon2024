ab1e392a783b104082b9f957f63582555761486b
pavelH <horejs.ho@gmail.com>
2024-05-26 22:09:48 +0200

    default language

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+module.exports = {
+  i18n: {
+    locales: ['cs', 'en'],
+    defaultLocale: 'cs',
+    localeDetection: false,
+  },
+  trailingSlash: true,
+}
 const nextConfig = {}
-  
-  module.exports = nextConfig
+module.exports = nextConfig
