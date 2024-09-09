0180c1c0ba5277a06d4ebbb1cd2e595fe80cda34
pavelH <horejs.ho@gmail.com>
2024-09-09 09:55:56 +0200

    fix

--- components/wroteaboutus/Wroteaboutus.jsx
+++ components/wroteaboutus/Wroteaboutus.jsx
@@ -6,10 +6,35 @@ import Link from "next/link";
 const Wroteaboutus = () => {
   const { t } = useTranslation();
   return (
-    <section className="max-w-7xl mx-auto px-2">
-      <h2 className="text-center pb-[40px]">{t("napsalionas")}</h2>
-      <div className="max-w-[500px] mx-auto">
+    <section className="mx-auto max-w-7xl px-2">
+      <h2 className="pb-[40px] text-center">{t("napsalionas")}</h2>
+      <div className="mx-auto grid grid-cols-1 gap-[16px] md:grid-cols-3">
         <Link href="https://mam.cz/novinky/online-a-social/influencer-marketing/2024-05/konference-influcon-ukaze-roli-influenceru-pri-budovani-znacky/">
+          <div className="image-box">
+            <Image src={Mam}></Image>
+          </div>
+        </Link>
+        <Link href="https://www.mediar.cz/kdo-v-influencer-marketingu-neco-znamena-bude-7-rijna-na-influconu/">
+          <div className="image-box">
+            <Image src={Mam}></Image>
+          </div>
+        </Link>
+        <Link href="https://tv.nova.cz/clanek/572146-v-praze-se-bude-v-rijnu-konat-nejvetsi-konference-o-influencer-marketingu-influcon">
+          <div className="image-box">
+            <Image src={Mam}></Image>
+          </div>
+        </Link>
+        <Link href="https://newsfeed.cz/v-praze-se-bude-v-rijnu-konat-nejvetsi-konference-o-influencer-marketingu-influcon/">
+          <div className="image-box">
+            <Image src={Mam}></Image>
+          </div>
+        </Link>
+        <Link href="https://www.webtop100.cz/inspirace/podcast-webtop100-petr-srna-o-influconu">
+          <div className="image-box">
+            <Image src={Mam}></Image>
+          </div>
+        </Link>
+        <Link href="https://mam.cz/novinky/komunita/akce/konference/2024-09/influcon-nejvetsi-konference-o-influencer-marketingu-bude-uz-v-rijnu/">
           <div className="image-box">
             <Image src={Mam}></Image>
           </div>
